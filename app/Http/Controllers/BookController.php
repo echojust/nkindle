040635c09b2a0447d0fbde919da83bd9d5e0a1f7
hs920519 <383748139@qq.com>
2018-04-26 00:47:22 +0800

    新增图书管理器

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by HuangSheng.
+ * User: my
+ * Date: 2018/4/26
+ * Time: 0:46
+ * Email:383748139@qq.com
+ */
+
+namespace App\Http\Controllers;
+
+
+class BookController
+{
+
+}
